fe89db2a5037848e3f51e32a4a9ec1c8bc1e5281
Camila Arce <133790127+cararcel@users.noreply.github.com>
2024-07-03 15:53:18 +0200

    add banner + fix footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import AppLayout from '../components/layouts/AppLayout';
 import Card from '../components/Card';
+import '../styles/home.scss';
 
 function Home() {
     return (
         <>
             <AppLayout>
-                <h1>Home</h1>
+                <div className="home-banner">
+                    <p className="home-banner_text">
+                        Chez vous, partout&nbsp;et&nbsp;ailleurs
+                    </p>
+                </div>
+
                 <div className="gallery">
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
                     <Card />
                 </div>
             </AppLayout>
